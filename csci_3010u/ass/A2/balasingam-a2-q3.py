03acb2e5c5ebf1effbfe887343731cabf1c5e8be
judacribz <sheron.balas@gmail.com>
2017-11-04 21:15:52 -0400

    finished q3 of a2. q4 functionality done. Need to display histograms for time and displacement

--- csci_3010u/ass/A2/balasingam-a2-q3.py
+++ csci_3010u/ass/A2/balasingam-a2-q3.py
@@ -1,19 +1,37 @@
-import pygame, sys
-import matplotlib.pyplot as plt
-import numpy as np
-import random
-import os
+import pygame, sys, random, os, numpy as np, matplotlib.pyplot as plt
 from scipy.integrate import ode
 
-
+# =============================================================================
+# Constants
+# =============================================================================
+WHITE = (255, 255, 255)
 WIN_HEIGHT = 500
 WIN_WIDTH = 500
+
 WALLS = ["left", "right", "top", "bottom"]
+NUM_DISKS = 10
+MULTIPLIER = 100.
+DT = 0.01
+
+TITLE    = 'COLLIDING RANDOM DISKS'
+ONE_LINE = '-'*55
+TWO_LINE = ONE_LINE + '\n' + ONE_LINE
+# =============================================================================
+# End of Constants=============================================================================
 
 
-def normalize(v):
-    return v / np.linalg.norm(v)
+# =============================================================================
+# Env Vars
+# =============================================================================
+paused = False
+in_windows = False
+# =============================================================================
+# End of Env Vars==============================================================
 
+
+# =============================================================================
+# Disk2D Class
+# =============================================================================
 class Disk2D(pygame.sprite.Sprite):
     
     def __init__(self, imgfile, radius, mass=1.0):
@@ -58,11 +76,19 @@ class Disk2D(pygame.sprite.Sprite):
 
     def pprint(self):
         print 'Disk', self.state
+# =============================================================================
+# End of Disk2D Class==========================================================
 
+
+# =============================================================================
+# World Class
+# =============================================================================
 class World:
 
     def __init__(self):
+        self.paused = paused
         self.disks = []
+        self.dt = DT
         self.e = 1. # Coefficient of restitution
 
     def add(self, imgfile, radius, mass=1.0):
@@ -80,13 +106,19 @@ class World:
             d.draw(screen)
 
     def update(self, dt):
-
         self.check_for_collision()
 
         for d in self.disks:
             d.update(dt)
 
-    
+    def pause(self):
+        self.paused = True
+
+    def resume(self):
+        self.paused = False
+
+    # Goes through all walls and disks and sends data to
+    # compute_collision_response
     def check_for_collision(self):
         for i in range(0, len(self.disks)):
 
@@ -102,7 +134,7 @@ class World:
                 if self.compute_collision_response(i, j):
                     break
 
-
+    # Sets collision response
     def compute_collision_response(self, i, j, wall=""):
         pos_i = np.array(self.disks[i].state[0:2])
         mass_i = self.disks[i].mass
@@ -142,8 +174,9 @@ class World:
             n_ij = normalize(diff_pos)
 
             # Set resulting velocity/velocities
-            if np.dot(relative_vel_ij, n_ij) < 0:
-                J = -(1+self.e) * np.dot(relative_vel_ij, n_ij) / mass_denom
+            dot_rel_vel = np.dot(relative_vel_ij, n_ij)
+            if dot_rel_vel < 0:
+                J = -(1+self.e) * dot_rel_vel / mass_denom
 
                 vel_i_aftercollision = vel_i + n_ij * J / mass_i
                 self.disks[i].set_vel(vel_i_aftercollision)
@@ -154,9 +187,78 @@ class World:
 
                 return True
         return False
+# =============================================================================
+# End of World Class===========================================================
 
 
+# =============================================================================
+# Helper Functions
+# =============================================================================
+# Gets unit vector
+def normalize(v):
+    return v / np.linalg.norm(v)
+
+# Clears and updates window screen
+def update(screen, world):
+    screen.fill(WHITE)
+    world.draw(screen)
+    world.update(world.dt)
+    pygame.display.update()
+
+# Clears terminal screen
+def clear_screen():
+    if in_windows:
+        os.system('cls')
+    else:
+        os.system('clear')
+
+# Prints usage
+def usage():
+    print TITLE
+    print TWO_LINE
+    print 'Usage:'
+    print ONE_LINE
+    print 'python *q3* <n_#_of_disks(optional)>'
+    print TWO_LINE
+    print 'During Simulation:'
+    print ONE_LINE
+    print 'Press (r) to start/resume simulation'
+    print 'Press (p) to pause simulation'
+    print 'Press (q) to quit simulation'
+    print 'Press (space) to step forward simulation when paused'
+    print TWO_LINE + '\n'
+# =============================================================================
+# End of Helper Functions======================================================
+
+
+# =============================================================================
+# Main Function
+# =============================================================================
 def main():
+    # clear terminal and print usage
+    if sys.platform == "win32":
+        in_windows = True
+    clear_screen()
+    usage()
+
+    n_disks = NUM_DISKS
+
+    # get number of disks from cmdline arg
+    error = False
+    num_args = len(sys.argv) - 1
+    if num_args == 1:
+        n_disks = int(sys.argv[1])
+        if n_disks < 1:
+            error = True
+            clear_screen()
+            print "Error: Number of disks must be greater than 0"
+    elif num_args > 1:
+        error = True
+        clear_screen()
+        usage()
+        
+    if error:
+        sys.exit(0)
 
    # initializing pygame
     pygame.init()
@@ -176,9 +278,9 @@ def main():
     vel = []
     rad = []
     mass = []
-    for i in range(0, 10):
+    for i in range(0, n_disks):
         # random radiuses
-        rad.append(int(100.*random.uniform(0.1, 0.2)))
+        rad.append(int(MULTIPLIER*random.uniform(0.1, 0.2)))
         
         # random positions
         pos.append(random.uniform(0 + rad[i], WIN_WIDTH - rad[i]))
@@ -187,7 +289,7 @@ def main():
         # random velocities
         vel_mag = 0
         while vel_mag == 0:
-            vel_mag = random.uniform(0, 1000)
+            vel_mag = MULTIPLIER*random.uniform(0, 10)
 
         x_sign = random.choice([-1, 1])
         y_sign = random.choice([-1, 1])
@@ -210,33 +312,41 @@ def main():
 
     world = World()
 
-    for i in range(0, len(disk_img)):
-        world.add(disk_img[i], rad[i], mass[i]).set_pos(pos[i:i+2]).set_vel(vel[i:i+2])
+    img_i = 0
+    for i in range(0, n_disks):
+        world.add(disk_img[img_i], rad[i], mass[i]).set_pos(pos[i:i+2]).set_vel(vel[i:i+2])
+        img_i +=1
+        if img_i == 10:
+            img_i = 0
     # -------------------------------------------------------------------------
 
-
-    dt = 0.01
 
     while True:
         # 30 fps
         clock.tick(30)
 
         event = pygame.event.poll()
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit(0)
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_p:
+            world.pause()
+            continue
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+            world.resume()
+            continue
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_q:
             pygame.quit()
-            sys.exit(0)
+            break
         else:
             pass
 
-        # Clear the background, and draw the sprites
-        screen.fill(WHITE)
-        world.draw(screen)
-        world.update(dt)
-
-        pygame.display.update()
+        # update simulation
+        if not world.paused:
+            # Clear the background, and draw the sprites
+            update(screen, world)
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                update(screen, world)
+# =============================================================================
+# End of Main Function=========================================================
 
 if __name__ == '__main__':
     main()
--- csci_3010u/ass/A2/balasingam-a2-q4.py
+++ csci_3010u/ass/A2/balasingam-a2-q4.py
@@ -1,36 +1,172 @@
-import random
-import numpy as np
+import random, sys, os, numpy as np
 
-def simulate_leaf_fall(h):
-    y = h
+# =============================================================================
+# Constants
+# =============================================================================
+HEIGHT = 100
+NUM_RUNS = 1000
+P_LEFT_OFFSET = 0
+
+# leaf motion probabilities
+P_UP = 0.1
+P_DOWN = 0.55
+P_LEFT = 0.15
+P_RIGHT = 0.15
+
+
+TITLE    = 'LEAF IN THE WIND'
+ONE_LINE = '-'*55
+TWO_LINE = ONE_LINE + '\n' + ONE_LINE
+# =============================================================================
+# End of Constants=============================================================================
+
+in_windows = False
+
+
+
+# =============================================================================
+# Leaf Fall Function
+# =============================================================================
+def simulate_leaf_fall(h, ranges):
     x = 0
+    time = 0
 
-    while y > 0:
+    while h > 0:
+        # assuming every change in position takes 1 second
+        time += 1
+
         change = random.uniform(0, 1)
 
-
         # P(0.1) of moving up
-        if change > 0.05 and change <= 0.15:
-            y += 1
+        if change <= ranges[0]:
+            h += 1
         # P(0.55) of moving down
-        elif change > 0.15 and change <= 0.70:
-            y -= 1
-        # P(0.15) of moving left
-        elif change > 0.70 and change <= 0.85:
+        elif change > ranges[0] and change <= ranges[1]:
+            h -= 1
+        # P(0.15 + P_LEFT_OFFSET) of moving left
+        elif change > ranges[1] and change <= ranges[2]:
             x -= 1
-        # P(0.15) of moving right
-        elif change > 0.85 and change <= 1.0:
+        # P(0.15 - P_LEFT_OFFSET) of moving right
+        elif change > ranges[2] and change <= ranges[3]:
             x += 1
         # P(0.05) of staying in the same spot
 
-    return x
+    return time, x
+# =============================================================================
+# End of Leaf Fall Function====================================================
 
-n = 1000
-sum = 0
-disp = []
-for h in range (100, 1000):
+
+# =============================================================================
+# Helper Functions
+# =============================================================================
+
+def get_prob_range(p):
+    # setup probability ranges from 0 to 1 in ranges list in the order
+    # top, bottom, left right
+    ranges = []
+    r = 0
+    for i in range(0, len(p)):
+        r += p[i]
+        ranges.append(round(r, 2))
+
+    return ranges
+
+# Clears terminal screen
+def clear_screen():
+    if in_windows:
+        os.system('cls')
+    else:
+        os.system('clear')
+
+# Prints usage
+def usage():
+    print TITLE
+    print TWO_LINE
+    print 'Usage:'
+    print ONE_LINE
+    print 'python *q4* <height(optional)>'
+    print '            <#_of_runs(optional)>'
+    print '            <left_probability_in[0, 0.3](optional)>'
+    print TWO_LINE
+# =============================================================================
+# End of Helper Functions======================================================
+
+
+
+def main():
+    # clear screen and print usage
+    if sys.platform == "win32":
+        in_windows = True
+    clear_screen()
+    usage()
+
+    h = HEIGHT
+    n = NUM_RUNS
+
+    # get cmdline args
+    num_args = len(sys.argv) - 1
+
+    p_left = P_LEFT
+    p_right = P_RIGHT
+
+    error = False
+    if num_args > 3:
+        error = True
+        clear_screen()
+        usage()
+    else:
+        # height input
+        if num_args >= 1:
+            h = int(sys.argv[1])
+
+            if h <= 0:
+                error = True
+                clear_screen()
+                print "Error: Height must be greater than 0"
+
+        # number of runs input
+        if num_args >= 2:
+            n = int(sys.argv[2])
+
+            if n <= 0:
+                error = True
+                clear_screen()
+                print "Error: Number of runs must be greater than 0"
+
+        # left movement probability input
+        if num_args == 3:
+            p_left = round(float(sys.argv[3]), 2)
+
+            if p_left < 0 or p_left > 0.3:
+                error = True
+                clear_screen()
+                print "Error: Left probability must be in [0, 0.3]"
+
+            p_right += round(P_LEFT - p_left, 2)
+
+    if error:
+            sys.exit(0)
+
+
+    probs = [P_UP, P_DOWN, p_left, p_right]
+    ranges = get_prob_range(probs)
+
+    order = "[top, down, left, right]"
+    print "Probabilities:"
+    print order + ": " + str(probs)
+    print "\nRange End:"
+    print order + ": " + str(ranges)
+
+    time = []
+    disp = []
+
     for i in range(0, n):
-        disp.append(simulate_leaf_fall(h))
-    print str(h) + ": " + str(np.var(disp))
+        t, x = simulate_leaf_fall(h, ranges)
+        time.append(t)
+        disp.append(x)
+    print "\n\nDisplacement:\tmean: " + str(np.mean(disp)) + "\tvariance: " + str(np.var(disp))
+    print "Time:\t\tmean: " + str(np.mean(time)) + "\tvariance: " + str(np.var(time))
 
 
+if __name__ == '__main__':
+    main()
