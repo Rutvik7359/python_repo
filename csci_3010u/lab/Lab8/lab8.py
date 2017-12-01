0f86d001b4e690f22247a49eefd6cf24c630d864
judacribz <sheron.balas@gmail.com>
2017-12-01 16:17:23 -0500

    finished lab 8 and report

--- csci_3010u/lab/Lab8/lab8.py
+++ csci_3010u/lab/Lab8/lab8.py
@@ -1,10 +1,10 @@
-import random, simpy, numpy as np, matplotlib.pyplot as plt
+import random, simpy, time, numpy as np, matplotlib.pyplot as plt
 from scipy.stats import norm
 
 
 RANDOM_SEED = 42
-NUM_WASH_STATIONS = 9 # Number of machines in the carwash
-NUM_DRY_STATIONS = 3 # Number of machines in the carwash
+NUM_WASH_STATIONS = 2 # Number of machines in the carwash
+NUM_DRY_STATIONS = 1 # Number of machines in the carwash
 ARRIVALTIME = [3, 4, 5, 3, 3, 4, 9, 10]
 WASHTIME = [5, 5, 5, 6, 6, 5, 5, 5, 5] # Minutes it takes to clean a car
 DRYTIME = [1.5, 1.5, 1.7, 1.5, 1.5, 1.5, 1.7, 1.5, 1.4] # Minutes it takes to clean a car
@@ -45,8 +45,10 @@ def car(env, name, cw, stats):
     and requests a cleaning machine. It then starts the washing process, waits
     for it to finish and leaves to never come back ...
     """
-    stats['cars'].append(name)
     arrival_time = env.now
+    name = name + " " + str(arrival_time)
+    stats['cars'].append(name)
+    
     with cw.wash_station.request() as request:
     	yield request
     	print('%s enters the car wash machine at %.2f.' % (name, env.now))
@@ -81,13 +83,13 @@ def setup(env, num_wash_stations, num_dry_stations, wash_time, dry_time, arrival
     carwash = Carwash(env, num_wash_stations, num_dry_stations, wash_time, dry_time)
     # Create 4 initial cars
     for i in range(4):
-	   env.process(car(env, 'Car %d' % i, carwash, stats))
+	   env.process(car(env, 'Car', carwash, stats))
 
     # Create more cars while the simulation is running
     while True:
 	   yield env.timeout(random.choice(arrival_time))
 	   i += 1
-	   env.process(car(env, 'Car %d' % i, carwash, stats))
+	   env.process(car(env, 'Car', carwash, stats))
 
 
 
@@ -104,6 +106,8 @@ env.run(until=SIM_TIME)
 	
 print 'stats'
 print stats
+print sum(stats['totaltimes'])/len(stats['totaltimes'])
+print sum(stats['waittimes'])/len(stats['waittimes'])
 
 plt.figure(1)
 plt.hist(stats['totaltimes'], color='crimson', edgecolor='black', linewidth=1.2)
@@ -113,6 +117,6 @@ plt.ylabel('Number of cars')
 plt.figure(2)
 plt.hist(stats['waittimes'], color='crimson', edgecolor='black', linewidth=1.2)
 plt.xlabel('Time (in minutes)')
-plt.title('Total time spent in the carwash')
+plt.title('Total wait time in the carwash')
 plt.ylabel('Number of cars')
 plt.show()
